807afd9b436b99213573d2363e3d4801e7c97ba9
Youngislander <yjmnopainnogain@gmail.com>
2020-11-13 00:33:42 +0900

    url

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,9 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-    uri: "https://ecodictionarydb-39a06133be.herokuapp.com",
+    uri: process.env.NODE_ENV === "development"
+    ? "http://localhost:4000"
+    : "https://prismagram-backend.herokuapp.com",
     clientState: {
         defaults,
         resolvers
