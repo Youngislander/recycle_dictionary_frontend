006371b3abe18b25518e759ca06fcb282b15e6c9
Youngislander <yjmnopainnogain@gmail.com>
2020-11-13 01:46:44 +0900

    end-point

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,13 +2,7 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-<<<<<<< HEAD
     uri: "http://localhost:4000",
-=======
-    uri: process.env.NODE_ENV === "development"
-    ? "http://localhost:4000"
-    : "https://ecodictionarydb-39a06133be.herokuapp.com/",
->>>>>>> aafc62ac7a49b4b42520198c1f703c8ea9d8f972
     clientState: {
         defaults,
         resolvers
